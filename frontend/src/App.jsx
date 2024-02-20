3a06ed58e3afa8de17014e505035c2e4d0df548f
Moahmmed Bilal Shaikh <bilalshaikh292@gmail.com>
2024-02-20 23:29:56 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import Home from "./components/Home.jsx";
 
 function App() {
   return (
-    <div className="text-red-500">
+    <div>
       <Home />
     </div>
   );
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,6 +2,7 @@ import background from "../assets/background.mp4";
 import axios from "axios";
 import { useState } from "react";
 import { useDropzone } from "react-dropzone";
+import RecommendationApp from "./Recommender.jsx";
 
 const Home = () => {
   const [isloading, setisloading] = useState(false);
@@ -15,8 +16,6 @@ const Home = () => {
     file: null,
   });
 
-  // const [files, setFiles] = useState([]);
-
   const onDrop = (acceptedFiles) => {
     const selectedFile = acceptedFiles[0];
     setFile(selectedFile);
@@ -27,7 +26,6 @@ const Home = () => {
     accept: "image/*",
   });
 
-  isDragActive;
   const handleChange = ({ currentTarget: input }) => {
     setData({ ...data, [input.name]: input.value });
   };
@@ -43,13 +41,13 @@ const Home = () => {
         setImageToShow(data.imageUrl);
       } else if (file) {
         const endpoint =
-          "https://gdsccustomvission-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/ffa9eb29-7aec-4195-bb1f-e44ae3c639fd/classify/iterations/GDSC%20X-ray%20model/image";
+          "Your private endpoint";
         const formData = new FormData();
         formData.append("image", file);
 
         const response = await axios.post(endpoint, formData, {
           headers: {
-            "Prediction-Key": "05a818d2768d4de39e1eb779332509b3",
+            "Prediction-Key": "Your prediction key",
             "Content-Type": "application/octet-stream",
           },
         });
@@ -70,6 +68,12 @@ const Home = () => {
     }
   };
 
+  const highestProbabilityDisease = predictions[0]
+    ? predictions.reduce((prev, current) =>
+        prev.probability > current.probability ? prev : current
+      ).tagName
+    : "";
+
   return (
     <div className="flex">
       {isloading ? (
@@ -82,9 +86,9 @@ const Home = () => {
         <div className="main">
           <div className="overlay"></div>
           <video src={background} autoPlay loop muted />
-          <div className="content gap-14">
+          <div className="text-3xl content gap-48">
             <div>
-              <h1 className="text-3xl text-center">Welcome</h1>
+              <h1 className="text-5xl text-center">Welcome</h1>
               <form
                 onSubmit={handleSubmit}
                 className="flex flex-col items-center"
@@ -95,57 +99,81 @@ const Home = () => {
                   name="imageUrl"
                   onChange={handleChange}
                   value={data.imageUrl}
-                  className="text-black py-3 px-2 w-[350px] rounded-md my-7 border-none outline-none"
+                  className="text-black py-3 px-2 w-[500px] rounded-md my-7 border-none outline-none text-base"
                 />
-                <h1 className="text-center my-3">OR</h1>
+                <h1 className="text-center mb-7">OR</h1>
 
                 <div {...getRootProps()} style={dropzoneStyles}>
                   <input {...getInputProps()} />
+
                   {isDragActive ? (
                     <p>Drop the files here ...</p>
                   ) : (
                     <p>Drag n drop some files here, or click to select files</p>
                   )}
                 </div>
-
                 {file && (
                   <div>
-                    <img className="my-5 rounded-lg w-[100px] h-[100px]" src={URL.createObjectURL(file)} alt="Selected" />
+                    <img
+                      className="my-5 rounded-lg w-[100px] h-[100px]"
+                      src={URL.createObjectURL(file)}
+                      alt="Selected"
+                    />
                   </div>
                 )}
-
                 {error && <div className="text-red-500">{error}</div>}
                 <button
                   type="submit"
-                  className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                  className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg px-5 py-2.5 text-center me-2 mb-2 text-2xl"
                 >
-                  Test
+                  Generate Result
                 </button>
               </form>
             </div>
-            <div>
-              <h2 className="text-xl mt-4 text-center">Results.</h2>
-              {predictions.length > 0 && (
+            {predictions.length > 0 && (
+              <div>
+                <h2 className="text-5xl font-bold text-center">Results</h2>
                 <img
                   src={imageToShow}
                   alt="Uploaded"
-                  className="my-4 w-[350px] h-[350px] object-contain"
+                  className="my-4 max-w-[500px] max-h-[500px] object-contain"
                 />
-              )}
 
-              <ul>
-                {predictions.map((prediction, index) => (
-                  <li key={index} className="text-center text-lg">
-                    {Math.round(prediction.probability * 100) > 0 && (
-                      <>
-                        <strong>{prediction.tagName}:</strong>{" "}
-                        {Math.round(prediction.probability * 100)}%
-                      </>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </div>
+                <ul>
+                  {predictions.map((prediction, index) => (
+                    <li key={index} className="text-2xl text-center">
+                      {Math.round(prediction.probability * 100) > 0 && (
+                        <>
+                          <div className="pb-4">
+                            <strong>{prediction.tagName}:</strong>{" "}
+                            {Math.round(prediction.probability * 100)}%
+                          </div>
+                          <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
+                            <div
+                              className="bg-blue-600 h-2.5 rounded-full dark:bg-blue-500"
+                              style={{
+                                width: `${Math.round(
+                                  prediction.probability * 100
+                                )}%`,
+                              }}
+                            ></div>
+                          </div>
+                        </>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+            {predictions.length > 0 && (
+              <div>
+                {!(highestProbabilityDisease == "") &&
+                  !(highestProbabilityDisease == "Normal Chest") &&
+                  !(highestProbabilityDisease == "Brain No Tumor") && (
+                    <RecommendationApp disease={highestProbabilityDisease} />
+                  )}
+              </div>
+            )}
           </div>
         </div>
       )}
@@ -160,7 +188,7 @@ const dropzoneStyles = {
   textAlign: "center",
   cursor: "pointer",
   width: "500px",
-  marginBottom:"20px"
+  marginBottom: "20px",
 };
 
 export default Home;
